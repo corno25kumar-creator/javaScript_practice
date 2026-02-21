4b94bc666bbe456be0dec207cad119c660fdac9e
corno25kumar-creator <corno25kumar@gmail.com>
2026-02-21 21:50:18 +0530

    add prototype origin , pollyfill

---
+++ prototype/pollyfill.js
@@ -0,0 +1,14 @@
+// object.create
+
+function creator() {
+    //new obj create
+    // newly created obj ko link
+    // parameter obj
+}
+
+const obj = {
+    name : "x"
+}
+
+const obj2 = Object.create(obj)
+console.log(obj2)
